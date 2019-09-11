5badb7f7d7803cb123e61c3646f0d64d3ddd69b1
Mikhail Burilov <burilovmv@gmail.com>
2019-09-11 08:28:01 +0300

    Fixed assembly resolve issues

--- SupplyCollectorDataLoader/Program.cs
+++ SupplyCollectorDataLoader/Program.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Reflection;
+using System.Runtime.Loader;
 using System.Text;
 using S2.BlackSwan.SupplyCollector;
 using S2.BlackSwan.SupplyCollector.Models;
 
 namespace SupplyCollectorDataLoader {
     public class Program {
+        private static bool _debug = false;
+
         static void Main(string[] args) {
             if (args.Length == 0 || args.Length != 5 || "--help".Equals(args[0]) || "/?".Equals(args[0])) {
                 Console.WriteLine("Usage: DataLoader <CollectorName> <ConnectionString> <DataCollection> <DataEntity> <SamplesCount>");
@@ -21,8 +25,11 @@ namespace SupplyCollectorDataLoader {
             var samplesCount = Int64.Parse(args[4]);
 
             string supplyCollectorPath = Path.Combine(Environment.CurrentDirectory, supplyCollectorName + ".dll");
+            if(_debug)
+                Console.WriteLine($"[DEBUG] Loading supply collector from {supplyCollectorPath}");
 
-            Assembly supplyCollectorAssembly = Assembly.LoadFile(supplyCollectorPath);
+            AssemblyLoadContext.Default.Resolving += Assembly_Resolving;
+            Assembly supplyCollectorAssembly = AssemblyLoadContext.Default.LoadFromAssemblyPath(supplyCollectorPath);
             Type supplyCollectorType = supplyCollectorAssembly.GetType(String.Format("{0}.{0}", supplyCollectorName));
             Type loaderType = supplyCollectorAssembly.GetType(String.Format("{0}.{0}Loader", supplyCollectorName));
 
@@ -56,6 +63,26 @@ namespace SupplyCollectorDataLoader {
             catch (Exception ex) {
                 Console.WriteLine($"Error loading data: {ex}");
             }
+        }
+
+        private static Assembly Assembly_Resolving(AssemblyLoadContext context, AssemblyName name)
+        {
+            if (_debug)
+                Console.WriteLine($"[DEBUG] Resolving {name.FullName}");
+
+            var foundDlls = Directory.GetFileSystemEntries(new FileInfo(Environment.CurrentDirectory).FullName, name.Name + ".dll", SearchOption.AllDirectories);
+            if (foundDlls.Any())
+            {
+                if (_debug) {
+                    foreach (var foundDll in foundDlls) {
+                        Console.WriteLine($"  resolved to {foundDll}");
+                    }
+                }
+
+                return context.LoadFromAssemblyPath(foundDlls[0]);
+            }
+
+            return context.LoadFromAssemblyName(name);
         }
     }
 }
